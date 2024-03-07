10ce12173fcfcbee7d84964d3505455e02413b85
Himanshu Gupta <himanshugupta543210@gmail.com>
2024-03-07 20:19:55 +0530

    commit

---
+++ TicTacToe/app.js
@@ -0,0 +1,6 @@
+let boxes = document.querySelector(".box");
+let reset = document.querySelector("#rest-btn");
+
+let turn0 = true; //playerX playerO
+
+let array2 = [["apple, Orange"], ["lichi, banana"], ["strawberry", "graphs"] ];
